462b12d16794be524bac84c909da9a637fdc828e
hantriseptiania <71365751+hantriseptiania@users.noreply.github.com>
2020-09-18 13:55:49 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var number = 3;
+var huruf ="hantri";
+var cantik= true;
+var jelek= null;
+var hantri;
+var tanggallahir = [13, 9, 1987]
+
+console.log (number);
+console.log (huruf);
+console.log (cantik);
+console.log (jelek);
+console.log (hantri);
+console.log (tanggallahir);
